272f0779f85536b8cc9f1312751f3614a703f376
Naim Baziz <naim.baziz220@gmail.com>
2024-11-13 13:11:12 +0700

    assignment 9 adjustment on main/views.py

--- main/views.py
+++ main/views.py
@@ -17,15 +17,13 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
 
-...
-
 
 @csrf_exempt
 def create_mood_flutter(request):
     if request.method == "POST":
 
         data = json.loads(request.body)
-        new_mood = MoodEntry.objects.create(
+        new_mood = ProductEntry.objects.create(
             user=request.user,
             mood=data["mood"],
             mood_intensity=int(data["mood_intensity"]),
